2a552abd5896d0a253a77248c03a898665ca1c7c
rookiezhou <mmix1009@163.com>
2014-06-09 09:46:23 +0800

    Initial Commit

---
+++ UMengDemo/O2OAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  O2OAppDelegate.h
+//  UMengDemo
+//
+//  Created by Marcus on 14-6-9.
+//  Copyright (c) 2014年 Marcus.ji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface O2OAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UMengDemoTests/UMengDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  UMengDemoTests.m
+//  UMengDemoTests
+//
+//  Created by Marcus on 14-6-9.
+//  Copyright (c) 2014年 Marcus.ji. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UMengDemoTests : XCTestCase
+
+@end
+
+@implementation UMengDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
