0137c6a311a779a6e77fe761d9bf1ee3aee781fa
rookiezhou <mmix1009@163.com>
2014-06-09 15:49:39 +0800

    删除了原来的分享方式

--- UMengDemo/HomeViewController.m
+++ UMengDemo/HomeViewController.m
@@ -19,7 +19,8 @@
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithNibName:nibNameOrNil
+                            bundle:nibBundleOrNil];
     if (self) {
         
     }
@@ -38,12 +39,17 @@
 }
 
 - (IBAction)shareButtonPressedEventHandle:(id)sender {
-    [UMSocialSnsService presentSnsIconSheetView:self
-                                         appKey:@"5395117356240b420e03ca2a"
-                                      shareText:@"你要分享的文字"
-                                     shareImage:[UIImage imageNamed:@"icon.png"]
-                                shareToSnsNames:[NSArray arrayWithObjects:UMShareToQQ,UMShareToQzone,UMShareToDouban,UMShareToRenren,nil]
-                                       delegate:self];
+    
+//    使用这种方法有一个非常不好的地方，因为UMShareToQQ对应的分享Logo可能因为用户没有安装qq app导致这个Logo完全是一个空白
+//
+//    [UMSocialSnsService presentSnsIconSheetView:self
+//                                         appKey:@"5395117356240b420e03ca2a"
+//                                      shareText:@"你要分享的文字"
+//                                     shareImage:[UIImage imageNamed:@"icon.png"]
+//                                shareToSnsNames:[NSArray arrayWithObjects:UMShareToQQ,UMShareToQzone,UMShareToDouban,UMShareToRenren,nil]
+//                                       delegate:self];
+//   [[UMSocialControllerService defaultControllerService] setShareText:@"分享内嵌文字" shareImage:[UIImage imageNamed:@"icon"] socialUIDelegate:self];        //设置分享内容和回调对象
+    
 
 }
 
